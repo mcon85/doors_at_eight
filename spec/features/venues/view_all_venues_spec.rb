9979e0593c5f68feacc09e1a20ecd5fbdc97d29a
Serge Pokrovskii <Pokrovskiy@Serges-MacBook-Pro.local>
2016-07-05 16:18:31 -0400

    fixed hound suggestions

--- spec/features/venues/view_all_venues_spec.rb
+++ spec/features/venues/view_all_venues_spec.rb
@@ -1,12 +1,12 @@
-require "rails_helper"
+require 'rails_helper'
 
-feature "View all venues" do
-  scenario "When user vists page see all venues." do
+feature 'View all venues' do
+  scenario 'When user vists page see all venues.' do
     venues = FactoryGirl.create_list(:venue, 3)
 
     visit venues_path
 
-    expect(page).to have_content("Venues")
+    expect(page).to have_content('Venues')
     venues.each do |venue|
       expect(page).to have_content(venue.name)
     end
--- spec/support/factories/venues.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :venue do
-    sequence(:name) {|number| "Venue #{number}" }
-    capacity 34
-    website "http://www.venue.com"
-    address "23 harisson ave"
-    t_accessible true
-  end
-end
