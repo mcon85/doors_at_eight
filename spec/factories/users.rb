22e81e9c3336d6acaa832c258ccb1b061f44d778
Hannah Read <hannahdotread+gituser@gmail.com>
2016-07-05 16:18:37 -0400

    Adding devise, tests for account creation and sign in

---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    email 'doorsat8@mailinator.com'
+    password 'please123'
+  end
+end
---
+++ spec/features/01_can_create_account_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+feature "can create a new user account" do
+  scenario 'visits sign up page' do
+    visit '/users/sign_up'
+    expect(page).to have_content "Sign up"
+  end
+
+  scenario 'creates an account' do
+    visit '/users/sign_up'
+    fill_in 'Email', with: "doorsat8@mailinator.com"
+    fill_in 'Password', with: "abc12345"
+    fill_in 'Password confirmation', with: "abc12345"
+    click_button "Sign up"
+    expect(page).to have_content I18n.t 'devise.registrations.signed_up'
+  end
+end
