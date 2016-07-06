e660b8d20b58885051a8b5c7a35b4d2faaa368b3
Serge Pokrovskii <Pokrovskiy@Serges-MacBook-Pro.local>
2016-07-06 10:26:45 -0400

    added venues details

--- spec/features/venues/view_venue_details_spec.rb
+++ spec/features/venues/view_venue_details_spec.rb
@@ -1,13 +1,22 @@
 require 'rails_helper'
 
 feature 'View details of the venue' do
-  scenario 'When a user clicks on the venue name see venue details.' do
-    venue = FactoryGirl.create(:venue)
+  let!(:venue) { FactoryGirl.create(:venue) }
 
+  scenario 'When a user clicks on the venue name see venue details.' do
     visit venues_path
 
     click_link(venue.name)
 
     expect(page).to have_content(venue.name)
+  end
+
+  scenario 'When user vists page should see venue details' do
+    visit venue_path(venue)
+
+    expect(page).to have_content(venue.name)
+    expect(page).to have_content(venue.website)
+    expect(page).to have_content(venue.address)
+    expect(page).to have_content(venue.capacity)
   end
 end
