88f2bff936d603b256d0f748a9c6b43d5d299cfe
Serge Pokrovskii <Pokrovskiy@Serges-MacBook-Pro.local>
2016-07-06 14:03:14 -0400

    Revert "Add Review model with validations"
    This reverts commit 9aad33b07d9982a448b0d237e887c86b3541d417.

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 describe Review, type: :model do
-  it { should have_valid(:rating).when(1, 2, 3, 4, 5, '', nil) }
-  it { should_not have_valid(:rating).when(0, 6, 10, 'abd', 'a') }
+  it { should have_valid(:rating).when(1..5, '', nil) }
+  it { should_not have_valid(:rating).when('abd', 'a') }
 
   it { should have_valid(:body).when('this is a review') }
   it { should_not have_valid(:body).when('', nil) }
