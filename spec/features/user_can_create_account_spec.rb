a591f4995d51b9a9c373cf5485fa2c823c778b0e
Hannah Read <hannahdotread+gituser@gmail.com>
2016-07-06 16:17:18 -0400

    Added persistent sign in and out links in nav bar

--- spec/features/user_can_create_account_spec.rb
+++ spec/features/user_can_create_account_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 feature 'User can create new account' do
   scenario 'visits sign up page' do
-    visit '/users/sign_up'
-    expect(page).to have_content 'Sign up'
+    visit root_path
+    expect(page).to have_content 'Sign Up'
   end
 
   scenario 'creates an account' do
-    visit '/users/sign_up'
+    visit root_path
 
+    click_link 'Sign Up'
     fill_in 'Email', with: 'doorsat8@mailinator.com'
     fill_in 'Password', with: 'abc12345'
     fill_in 'Password confirmation', with: 'abc12345'
