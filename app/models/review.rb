cc6765283a2bcb972361338862976d75819b77e4
Serge Pokrovskii <Pokrovskiy@Serges-MacBook-Pro.local>
2016-07-06 14:31:43 -0400

    fixed the spacing

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,10 +2,10 @@ class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :venue
 
-  validates :rating, numericality: { only_integer: true,
+  validates :rating, allow_blank: true,
+                     allow_nil: true,
+                     numericality: { only_integer: true,
                                      greater_than_or_equal_to: 1,
                                      less_than_or_equal_to: 5 },
-                     allow_blank: true,
-                     allow_nil: true
   validates :body, presence: true
 end
