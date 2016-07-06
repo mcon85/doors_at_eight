c31f6a3896a5e6902db577d3cd2a6b27c2b14fcf
Hannah Read <hannahdotread+gituser@gmail.com>
2016-07-06 15:17:29 -0400

    Fixing hound whitespace violation in sign out spec

--- spec/features/user_can_sign_out_spec.rb
+++ spec/features/user_can_sign_out_spec.rb
@@ -13,7 +13,7 @@ feature 'can sign out of an account', :devise do
     expect(page).to have_content I18n.t 'devise.sessions.signed_in'
 
     click_link 'Sign out'
-    
+
     expect(page).to have_content I18n.t 'devise.sessions.signed_out'
   end
 end
