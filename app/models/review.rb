31c0f0a3ce1eac784acae9b8d739365edf5159fe
Serge Pokrovskii <Pokrovskiy@Serges-MacBook-Pro.local>
2016-07-06 14:36:58 -0400

    fixed the comma that was in the rong place

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,6 +6,6 @@ class Review < ActiveRecord::Base
                      allow_nil: true,
                      numericality: { only_integer: true,
                                      greater_than_or_equal_to: 1,
-                                     less_than_or_equal_to: 5 },
+                                     less_than_or_equal_to: 5 }
   validates :body, presence: true
 end
