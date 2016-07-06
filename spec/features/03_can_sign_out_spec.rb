f3c7d29be54121ed0ddb7c3417a62e68bbb04d54
Hannah Read <hannahdotread+gituser@gmail.com>
2016-07-06 10:32:30 -0400

    Adding sign out tests

---
+++ spec/features/03_can_sign_out_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+class PostsTests < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+end
+
+feature 'can sign out of an account', :devise do
+  scenario 'can sign out' do
+    user = FactoryGirl.create(:user)
+    visit '/users/sign_in'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+    expect(page).to have_content I18n.t 'devise.sessions.signed_in'
+    click_link 'Sign out'
+    expect(page).to have_content I18n.t 'devise.sessions.signed_out'
+  end
+end
