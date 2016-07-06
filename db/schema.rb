c975a8dc18d36f05d9c40090a340665fd375eb74
Hannah Read <hannahdotread+gituser@gmail.com>
2016-07-06 11:24:14 -0400

    Satisfying hound hunger for single quotes

--- db/schema.rb
+++ db/schema.rb
@@ -34,15 +34,4 @@ ActiveRecord::Schema.define(version: 20160705185332) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "venues", force: :cascade do |t|
-    t.string  "name",                         null: false
-    t.integer "capacity"
-    t.string  "website"
-    t.string  "address",                      null: false
-    t.boolean "t_accessible", default: false
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
 end
