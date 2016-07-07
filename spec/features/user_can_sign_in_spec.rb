040ad8dca22c471d364f8222b700db133185cc38
Hannah Read <hannahdotread+gituser@gmail.com>
2016-07-07 14:43:04 -0400

    Generalized auth helper to take a passed in user

--- spec/features/user_can_sign_in_spec.rb
+++ spec/features/user_can_sign_in_spec.rb
@@ -2,14 +2,16 @@ require 'rails_helper'
 
 feature 'can sign in with an existing account', :devise do
   scenario 'signs in with an account' do
-    login_user
+    new_user = FactoryGirl.create(:user)
+    login_user(new_user)
     expect(page).to have_content I18n.t 'devise.sessions.signed_in'
   end
 end
 
 feature 'authenication helper works' do
   scenario 'we try to run this helper' do
-    user = login_user
-    expect(user).to_not eq(nil)
+    new_user = FactoryGirl.create(:user)
+    authed_user = login_user(new_user)
+    expect(authed_user).to_not eq(nil)
   end
 end
