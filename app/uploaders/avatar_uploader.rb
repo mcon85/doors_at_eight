bf93f6ea77a4647e0bdcbe6accb0a24008cf5351
Hannah Read <hannahdotread+gituser@gmail.com>
2016-07-07 12:04:07 -0400

    Fixing hound violations in css

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,6 +1,5 @@
 # encoding: utf-8
 class AvatarUploader < CarrierWave::Uploader::Base
-
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
