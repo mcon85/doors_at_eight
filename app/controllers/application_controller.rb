c6b9fe2d49f5ae76f30b15f3dd1146d5dc873beb
Hannah Read <hannahdotread+gituser@gmail.com>
2016-07-07 09:43:11 -0400

    Fixing indent violations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
-    protected
+  protected
 
-   def configure_permitted_parameters
+  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
       user_params.permit({ roles: [] }, :username, :email, :password, :password_confirmation)
     end
