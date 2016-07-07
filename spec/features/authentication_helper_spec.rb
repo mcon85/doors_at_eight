03c5ce280924232a36214f97b608a6ba8fb815e0
Nick(Mac) <nvanselow@hotmail.com>
2016-07-07 17:07:18 -0400

    hound comments

--- spec/features/authentication_helper_spec.rb
+++ spec/features/authentication_helper_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'authenication helper works' do
   let(:user) { FactoryGirl.create(:user) }
-  
+
   scenario 'we try to run this helper' do
     authed_user = login_user(user)
     expect(authed_user).to_not eq(nil)
