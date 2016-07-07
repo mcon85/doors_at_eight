9f584959de89edb969319b242afc12da05bf053a
Nick(Mac) <nvanselow@hotmail.com>
2016-07-07 17:13:03 -0400

    Hound fixes

--- spec/features/authentication_helper_spec.rb
+++ spec/features/authentication_helper_spec.rb
@@ -4,7 +4,6 @@ feature 'authenication helper works' do
   let(:user) { FactoryGirl.create(:user) }
 
   scenario 'we try to run this helper' do
-    authed_user = login_user(user)
-    expect(authed_user).to_not eq(nil)
+    expect { login_user(user) }.not_to raise_error
   end
 end
