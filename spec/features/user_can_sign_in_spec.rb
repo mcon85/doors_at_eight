561e968521cb11e42a5e2aedb2467d0bb524d527
Hannah Read <hannahdotread+gituser@gmail.com>
2016-07-07 11:50:46 -0400

    Adding tests for auth helper method

--- spec/features/user_can_sign_in_spec.rb
+++ spec/features/user_can_sign_in_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 feature 'can sign in with an existing account', :devise do
   scenario 'signs in with an account' do
-    user = FactoryGirl.create(:user)
-
-    visit root_path
-    click_link 'Sign In'
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_button 'Log in'
-
+    login_user
     expect(page).to have_content I18n.t 'devise.sessions.signed_in'
   end
+
+feature 'authenication helper works' do
+  scenario 'we try to run this helper' do
+    user = login_user
+    expect(user).to_not eq(nil)
+  end
+end
 end
