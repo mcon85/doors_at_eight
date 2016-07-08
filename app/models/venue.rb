063830ac6ffb3c4224623d9bce037c284a863906
Serge Pokrovskii <spokrovskey@gmail.com>
2016-07-08 13:31:45 -0400

    venue validations

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,16 @@
 class Venue < ActiveRecord::Base
+  has_many :reviews, dependent: :destroy
+
+  validates :name, presence: true
+  validates :capacity, allow_blank: true,
+                       allow_nil: true,
+                       numericality: { only_integer: true }
+  validates :website, allow_blank: true,
+                      allow_nil: true,
+                      format: { with: URI.regexp }
+  validates :address, presence: true
+  validates :t_accessible, inclusion: { in: [true, false] }
+
   def display_t_accessible
     if t_accessible
       'T is nearby'
