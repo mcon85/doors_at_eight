ffb7c36ab4619274a40587717e877d9be96b2fd7
Nick(Mac) <nvanselow@hotmail.com>
2016-07-08 09:39:33 -0400

    Fix line too long

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -18,7 +18,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
   #
-  # ActionController::Base.helpers.asset_path("fallback/" + [version_name,"default.png"].compact.join('_'))
+  # ActionController::Base.helpers.asset_path("fallback/" +
+  # [version_name,"default.png"].compact.join('_'))
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
