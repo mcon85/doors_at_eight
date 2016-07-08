88c42d709d04ec89236c13f3fda65bfe5a3ee76b
Maggie Concannon <concannon.maggie@gmail.com>
2016-07-08 16:29:02 -0400

    updated review feature spec

--- spec/features/reviews/create_new_review_spec.rb
+++ spec/features/reviews/create_new_review_spec.rb
@@ -8,16 +8,14 @@ feature 'Create a new review' do
     visit new_venue_review_path(venue.id)
   end
 
-  scenario 'User fills out review form correctly' do
-    it 'shows success message and displays new review on venue page' do
-      fill_in('Rating', with: 5)
-      fill_in('Body', with: 'this place is the shit')
+  scenario 'User sees success msg and updated page after filling out form' do
+    fill_in('Rating', with: 5)
+    fill_in('Body', with: 'this place is the shit')
 
-      click_button('Create Rating')
+    click_button('Create Rating')
 
-      expect(page).to have_content('Review added successfully')
-      expect(page).to have_content('5')
-      expect(page).to have_content('this place is the shit')
-    end
+    expect(page).to have_content('Review added successfully')
+    expect(page).to have_content('5')
+    expect(page).to have_content('this place is the shit')
   end
 end
