112cdeb74322c600eced78bdb6db45a600fa68ba
Maggie Concannon <concannon.maggie@gmail.com>
2016-07-08 17:20:08 -0400

    added additional review feature tests

--- spec/features/reviews/create_new_review_spec.rb
+++ spec/features/reviews/create_new_review_spec.rb
@@ -18,4 +18,31 @@ feature 'Create a new review' do
     expect(page).to have_content('5')
     expect(page).to have_content('this place is the shit')
   end
+
+  scenario 'User sees error msg if non-number entered' do
+    fill_in('Rating', with: 'b')
+
+    click_button('Create Rating')
+
+    expect(page).to have_content('Problem saving review.')
+    expect(page).to have_content('Rating is not a number')
+  end
+
+  scenario 'User sees error msg if blanks entered' do
+    click_button('Create Rating')
+
+    expect(page).to have_content('Problem saving review.')
+    expect(page).to have_content("Rating can't be blank")
+  end
+
+  scenario 'User sees error msg if blanks entered' do
+    fill_in('Rating', with: 8)
+
+    click_button('Create Rating')
+
+    expect(page).to have_content('Problem saving review.')
+    expect(page).to have_content("Rating must be less than or equal to 5")
+  end
+
+
 end
