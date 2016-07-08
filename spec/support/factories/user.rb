dd9decda9e9824831166a912bc1a2ded3907ad3e
Serge Pokrovskii <spokrovskey@gmail.com>
2016-07-08 11:18:10 -0400

    spaces are fixed

--- spec/support/factories/user.rb
+++ spec/support/factories/user.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:username){ |number| "User_#{number}" }
-    sequence(:email){ |number| "Email_#{number}@mailinator.com" }
+    sequence(:username) { |number| "User_#{number}" }
+    sequence(:email) { |number| "Email_#{number}@mailinator.com" }
     password 'password'
-    avatar File.open("#{Rails.root}/spec/support/images/sample_avatar.jpg") 
+    avatar File.open("#{Rails.root}/spec/support/images/sample_avatar.jpg")
     role 'member'
   end
 end
