665c2bf72fe9c194d85e79528dca2405494d0ccc
Serge Pokrovskii <spokrovskey@gmail.com>
2016-07-08 10:51:33 -0400

    Move factory to correct folder

--- spec/features/avatar_spec.rb
+++ spec/features/avatar_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
 feature 'avatar' do
+  let(:user) { FactoryGirl.build(:user) }
+
   scenario 'user uploads an avatar' do
     visit root_path
     click_link 'Sign Up'
 
-    fill_in 'Username', with: 'Test User'
-    fill_in 'Email', with: 'doorsat8@mailinator.com'
-    fill_in 'Password', with: 'thispasswordisawful'
-    fill_in 'Password confirmation', with: 'thispasswordisawful'
-    attach_file :user_avatar,
-                "#{Rails.root}/spec/support/images/sample_avatar.jpg"
+    fill_in 'Username', with: user.username
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password confirmation', with: user.password
+    attach_file :user_avatar, user.avatar
+
     click_button 'Sign up'
 
     expect(page).to have_content I18n.t 'devise.registrations.signed_up'
