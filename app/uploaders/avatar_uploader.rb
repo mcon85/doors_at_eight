e7ff95d557a118c6de0285ba3084022389cec56b
Nick(Mac) <nvanselow@hotmail.com>
2016-07-08 09:38:40 -0400

    fix hound violations

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -42,7 +42,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # end
 
   # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
+  # Avoid using model.id or version_name here, see uploader/store.rb for
+  # details.
   # def filename
   #   "something.jpg" if original_filename
   # end
