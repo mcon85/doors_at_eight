8c96f36b7db0ddc0880a38ef2971bcae44b20195
Maggie Concannon <concannon.maggie@gmail.com>
2016-07-08 16:14:41 -0400

    updated venue view to move errors to top and permitted avatar update on user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,5 +16,13 @@ class ApplicationController < ActionController::Base
                          :password_confirmation,
                          :avatar)
     end
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|                       user_params.permit({ roles: [] },
+                       :username,
+                       :email,
+                       :password,
+                       :password_confirmation,
+                       :current_password,
+                       :avatar)
+    end
   end
 end
