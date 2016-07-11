f10d40f0747966d62ab1f87109993a761d6a67a7
Maggie Concannon <concannon.maggie@gmail.com>
2016-07-11 11:47:09 -0400

    adding main.js

--- react/src/main.js
+++ react/src/main.js
@@ -1,10 +1,14 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import RecentReviews from './components/RecentReviews.js';
 
 $(function() {
-  ReactDOM.render(
-    <h1>Boo yaa</h1>,
-    document.getElementById('app')
-  );
+  let recentReviewsElement = document.getElementById('review-carousel')
+  if(recentReviewsElement){
+    ReactDOM.render(
+      <RecentReviews />,
+      recentReviewsElement
+    );
+  }
 });
