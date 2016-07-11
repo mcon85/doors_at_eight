4502273af5c9d05f365b6a44e5f92f5f28c513a9
Maggie Concannon <concannon.maggie@gmail.com>
2016-07-11 16:03:41 -0400

    setup for react component test

---
+++ spec/features/homepage/user_visits_homepage_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+feature 'user sees most recent reviews', js: true do
+  
+
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'capybara/poltergeist'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
@@ -26,7 +27,7 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
-
+Capybara.javascript_driver = :poltergeist
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
