6a25e7c4bc573681c3157b276b082732fdb0a941
Hannah Read <hannahdotread+gituser@gmail.com>
2016-07-12 13:24:07 -0400

    Adding tests for editing a review

--- spec/features/reviews/edit_review_spec.rb
+++ spec/features/reviews/edit_review_spec.rb
@@ -3,27 +3,21 @@ require 'rails_helper'
 feature 'Edit a review' do
   let(:venue) { FactoryGirl.create(:venue) }
   let(:user) { FactoryGirl.create(:user) }
-  let(:review) { FactoryGirl.create(:review) }
+  let!(:review) { FactoryGirl.create(:review, venue: venue) }
 
   before do
-    visit new_venue_review_path(venue.id)
+    login_user(user)
+    visit edit_venue_review_path(venue, review)
   end
 
   scenario 'User edits a review' do
-    visit edit_venue_review_path(venue, review)
-
     select('3', from: 'Rating')
     fill_in('Body', with: 'This review has been edited')
     click_button('Save Review')
 
-    expect(page).to have_content('3')
+    within('.review-rating') do
+      page.body.should have_content ('3')
+    end
     expect(page).to have_content('This review has been edited')
-  end
-
-  scenario 'User sees error msg if blanks entered' do
-    click_button('Save Review')
-
-    expect(page).to have_content('Problem saving review.')
-    expect(page).to have_content("Rating can't be blank")
   end
 end
