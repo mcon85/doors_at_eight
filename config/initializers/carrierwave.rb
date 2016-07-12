dfead57e6e1ca56777fcd21a5de8dedac04a2cb6
Nick(Mac) <nvanselow@hotmail.com>
2016-07-12 12:41:27 -0400

    Only use AWS for production

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,5 @@
 CarrierWave.configure do |config|
-  if Rails.env.production? || Rails.env.development?
+  if Rails.env.production?
     config.fog_credentials = {
       provider: "AWS",
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
