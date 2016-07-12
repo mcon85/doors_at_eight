8e789519eb7f7e4be6005b1b9ede3c3bc648ad75
Arber Theodhor <ath2585@gmail.com>
2016-07-12 14:30:48 -0400

    we added a button for reviews and testing for that feature

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,9 @@ Rails.application.routes.draw do
                             :create,
                             :update,
                             :destroy] do
-    resources :reviews
+    resources :reviews, only: [:index, :new, :create, :edit, :update]
   end
+  resources :reviews, only: [:destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- spec/features/reviews/delete_reviews_spec.rb
+++ spec/features/reviews/delete_reviews_spec.rb
@@ -5,17 +5,23 @@ feature 'Delete a review' do
   let!(:venue) { FactoryGirl.create(:venue) }
   let!(:review) { FactoryGirl.create(:review, venue: venue) }
 
-  before do
+  scenario 'User deletes review' do
     login_user(user)
     visit venue_path(venue)
-  end
 
-  scenario 'User deletes review' do
     within('.reviews') do
       click_button('Delete')
     end
 
     expect(page).not_to have_content(review.body)
     expect(page).to have_content('Review deleted successfully')
+  end
+
+  scenario 'Unauthenticated user' do
+    visit venue_path(venue)
+
+    within('.reviews') do
+      expect(page).not_to have_button('Delete')
+    end
   end
 end
