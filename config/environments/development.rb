cc2298183cfe8e32c59fc318ab7e3e9708088634
Maggie Concannon <concannon.maggie@gmail.com>
2016-07-13 15:29:40 -0400

    fixing hound violations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,8 +16,8 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
-  address: 'localhost',
-  port: 1025
+    address: 'localhost',
+    port: 1025
   }
 
   # Print deprecation notices to the Rails logger.
@@ -46,8 +46,8 @@ Rails.application.configure do
   # Default URL options for devise
 
   config.action_mailer.default_url_options = {
-  host: 'localhost',
-  port: 3000
+    host: 'localhost',
+    port: 3000
   }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  #Maggie adding devise mailer things
-  config.action_mailer.default_url_options = { :host => 'doors-at-eight.herokuapp.com'}
-
+  config.action_mailer.default_url_options = { host:
+                                              'doors-at-eight.herokuapp.com' }
 
 end
