ade35f7d1c94b71c735d751cf06db777dbd9f3d3
Nick(Mac) <nvanselow@hotmail.com>
2016-07-13 11:35:46 -0400

    added tests for chcking if a user is going to delete his own review and venue

---
+++ spec/features/reviews/other_people_cannot_edit_your_reviews_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+feature 'users cannot edit reviews they did not create', %{
+  As user
+  I can't edit other users' reviews
+  So that I don't mess up other users opinion
+} do
+  #ACCEPTANCE CRITERIA
+  # [X] User cannot edit another user's review
+  # [X] User cannot delete another user's review
+  # [X] User cannot visit edit form
+  # [X] User cannot delete reviews for a venue they created
+
+  let(:user) { FactoryGirl.create(:user) }
+  let(:another_user) { FactoryGirl.create(:user) }
+  let(:review) { FactoryGirl.create(:review, user: another_user) }
+
+  before do
+    login_user(user)
+    visit venue_path(review.venue)
+  end
+
+  scenario 'user tries to edit a review of another user' do
+    within('.reviews') do
+      expect(page).not_to have_button('Edit')
+    end
+  end
+
+  scenario 'users tries to visit the edit page for another user review' do
+    visit edit_review_path(review)
+
+    expect(page).to have_content('You do not have permission to complete '\
+                                 'that action.')
+    expect(current_path).to eq(root_path)
+    expect(page).not_to have_css('form')
+  end
+
+  scenario 'users tries to delete vanue of another user' do
+    within('.reviews') do
+      expect(page).not_to have_button('Delete')
+    end
+  end
+
+  scenario 'user tries to delete reviews associated with a venue they made' do
+    venue = FactoryGirl.create(:venue, user: user)
+    review = FactoryGirl.create(:review, user: another_user, venue: venue)
+
+    within('.reviews') do
+      expect(page).not_to have_button('Delete')
+    end
+  end
+end
