e63431befa58d84287beb4cd25cf6632ba4712cc
Nick(Mac) <nvanselow@hotmail.com>
2016-07-13 12:49:35 -0400

    Add ENV variables to pusher setup

---
+++ spec/support/mocks/fake_pusher.rb
@@ -0,0 +1,22 @@
+class FakePusher
+  def initialize(channel, event, pusher = FakePusher)
+  end
+
+  def self.trigger(channel, event, data)
+  end
+
+  def self.app_id=(id)
+  end
+
+  def self.key=(key)
+  end
+
+  def self.secret=(secret)
+  end
+
+  def self.logger=(logger)
+  end
+
+  def self.encrypted=(encrypted)
+  end
+end
--- spec/support/mocks/pusher.rb
+++
@@ -1,7 +0,0 @@
-class FakePusher
-  def initialize(channel, event, pusher = FakePusher)
-  end
-
-  def self.trigger(channel, event, data)
-  end
-end
