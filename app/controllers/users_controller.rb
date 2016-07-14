048ea5f674b187794f9a37ea38b421bab790faef
Hannah Read <hannahdotread+gituser@gmail.com>
2016-07-14 09:20:30 -0400

    fixing 1 hound violation made 5 more

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :authorize_user, {except: :show}
-  before_action :authenticate_user!, {except: :index}
+  before_action :authorize_user, except: :show
+  before_action :authenticate_user!, except: :index
 
   def index
     @admin_users = User.all
