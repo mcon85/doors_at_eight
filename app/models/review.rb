1c95e6d9dfcf84bbafd1097a96724df305816ca7
Nick(Mac) <nvanselow@hotmail.com>
2016-07-14 16:56:26 -0400

    Add vote_counts to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,4 +7,14 @@ class Review < ActiveRecord::Base
                      numericality: { only_integer: true,
                                      greater_than_or_equal_to: 1,
                                      less_than_or_equal_to: 5 }
+
+  def vote_count
+    votes.all.reduce(0) do |total, vote|
+      if(vote.vote_type == 'up')
+        total + 1
+      else
+        total - 1
+      end
+    end
+  end
 end
