1e1a6f11fb9b493bbd0249379e51dd862b9d353e
Hannah Read <hannahdotread+gituser@gmail.com>
2016-07-14 16:48:27 -0400

    allowing admins to delete users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
 
-  has_many :venues
-  has_many :reviews
-  has_many :votes
+  has_many :venues, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+  has_many :votes, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
