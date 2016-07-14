bc23eae97e4a027617a94d412228b6671818eb04
Serge Pokrovskii <spokrovskey@gmail.com>
2016-07-14 17:57:28 -0400

    fixed hound violation

--- spec/controllers/api_reviews_controler_spec.rb
+++ spec/controllers/api_reviews_controler_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Api::ReviewsController, type: :controller do
-   describe 'GET /api/reviews' do
+ describe 'GET /api/reviews' do
    it ' retrieves the 4 reviews' do
 
-   create_list(:review, 4)
-   reviews = create_list(:review, 4)
+     create_list(:review, 4)
+     reviews = create_list(:review, 4)
 
-    get :index
-    json = JSON.parse(response.body)
-    ids = json['reviews'].map { |c| c['id'] }
+     get :index
+     json = JSON.parse(response.body)
+     ids = json['reviews'].map { |c| c['id'] }
 
-    expect(response.content_type).to eq('application/json')
-    expect(response).to have_http_status(:ok)
-    expect(ids).to match_array(reviews.map(&:id))
+     expect(response.content_type).to eq('application/json')
+     expect(response).to have_http_status(:ok)
+     expect(ids).to match_array(reviews.map(&:id))
     end
   end
 end
