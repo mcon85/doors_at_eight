439a17f22ced967c9f4583d3d12eae40a36a52a1
Nick(Mac) <nvanselow@hotmail.com>
2016-07-15 09:40:51 -0400

    extract votes_controller stuff to the model

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,30 +3,10 @@ class VotesController < ApplicationController
 
   def create
     @review = Review.find(params[:review_id])
-    vote = Vote.where(user: current_user, review: @review).first
-
-    success_message = "Thanks for #{params[:vote_type]}-voting!"
-
-    if vote
-      if vote.vote_type == params[:vote_type]
-        vote.destroy
-        flash[:success] = 'Your vote was removed.'
-      else
-        vote.update(vote_type: params[:vote_type])
-        flash[:success] = success_message
-      end
-    else
-      vote = Vote.new(user: current_user,
-                      review: @review,
-                      vote_type: params[:vote_type])
-
-      if vote.save
-        flash[:success] = success_message
-      else
-        flash[:alert] = 'There was a problem saving that upvote.'
-      end
-    end
-
+    vote = Vote.create_or_cancel(current_user.id,
+                                 params[:review_id],
+                                 params[:vote_type])
+    flash[:success] = vote.message
     redirect_to venue_path(@review.venue)
   end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -6,4 +6,44 @@ class Vote < ActiveRecord::Base
   validates :vote_type, presence: true, inclusion: { in: VOTES }
   validates :user_id, presence: true
   validates :review_id, presence: true
+
+  def self.create_or_cancel(user_id, review_id, vote_type)
+    vote = Vote.where(user_id: user_id, review: review_id).first
+
+    success_message = "Thanks for #{vote_type}-voting!"
+
+    if vote
+      if vote.vote_type == vote_type
+        vote.destroy
+        vote.message = 'Your vote was removed.'
+      else
+        vote.update(vote_type: vote_type)
+        vote.message = success_message
+      end
+    else
+      vote = Vote.new(user_id: user_id,
+                      review_id: review_id,
+                      vote_type: vote_type)
+
+      if vote.save
+        vote.message = success_message
+      else
+        vote.message = 'There was a problem saving that upvote.'
+      end
+    end
+
+    if vote.destroyed?
+      nil
+    else
+      vote
+    end
+  end
+
+  def message=(text)
+    @message = text
+  end
+
+  def message
+    @message
+  end
 end
