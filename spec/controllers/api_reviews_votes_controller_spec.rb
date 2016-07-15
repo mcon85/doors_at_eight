bd48b9712881394bf99ab33b301b4065f889bfc8
Hannah Read <hannahdotread+gituser@gmail.com>
2016-07-15 11:13:00 -0400

    adding test for create route

--- spec/controllers/api_reviews_votes_controller_spec.rb
+++ spec/controllers/api_reviews_votes_controller_spec.rb
@@ -22,5 +22,27 @@ describe Api::Reviews::VotesController do
       expect(json['vote']['vote_type']).to eq(vote.vote_type)
       expect(json['vote_count']).to eq(1)
     end
+
+    describe 'POST #create' do
+      it 'posts a new vote' do
+        user = FactoryGirl.create(:user)
+        review = FactoryGirl.create(:review)
+        vote = FactoryGirl.build(:vote, user_id: user.id, review_id: review.id, vote_type: 'up')
+
+        allow(request.env['warden']).to receive(:authenticate!).and_return(user)
+        allow(controller).to receive(:current_user).and_return(user)
+
+        post :create, review_id: review.id, vote_type: vote.vote_type
+        json = JSON.parse(response.body)
+
+        expect(response.content_type).to eq('application/json')
+        expect(response).to have_http_status(:ok)
+        expect(json['vote']['id']).to be_truthy
+        expect(json['vote']['user_id']).to eq(vote.user_id)
+        expect(json['vote']['review_id']).to eq(vote.review_id)
+        expect(json['vote']['vote_type']).to eq(vote.vote_type)
+        expect(json['vote_count']).to eq(1)
+      end
+    end
   end
 end
