008776f6d5b5118a41bb11f122cd10c564c31b19
Nick(Mac) <nvanselow@hotmail.com>
2016-07-14 23:27:46 -0400

    hound violations

--- lib/twitter_service.rb
+++ lib/twitter_service.rb
@@ -6,10 +6,10 @@ class TwitterService
 
   def initialize
     @twitter = client.new do |config|
-                 config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
-                 config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
-                 config.access_token = ENV['TWITTER_ACCESS_TOKEN']
-                 config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
+      config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
+      config.consumer_secret = ENV['TWITTER_CONSUMER_SECRET']
+      config.access_token = ENV['TWITTER_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TWITTER_ACCESS_SECRET']
     end
   end
 
